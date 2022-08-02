ddf061a363a1cc9c12d3289c7040533034287f63
Josh Bowen <jskybowen@gmail.com>
2022-08-02 12:28:14 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/big"
+
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+func toBlockNumArg(number *big.Int) string {
+	if number == nil {
+		return "latest"
+	}
+	pending := big.NewInt(-1)
+	if number.Cmp(pending) == 0 {
+		return "pending"
+	}
+	return hexutil.EncodeBig(number)
+}
+
+func getBlockHeaderByNum(client *rpc.Client, args ...interface{}) (*ethtypes.Header, error) {
+	var raw json.RawMessage
+	err := client.CallContext(context.Background(), &raw, "eth_getBlockByNumber", args...)
+	if err != nil {
+		return nil, err
+	} else if len(raw) == 0 {
+		return nil, ethereum.NotFound
+	}
+	// Decode header and transactions.
+	var body ethtypes.Header
+	if err := json.Unmarshal(raw, &body); err != nil {
+		return nil, err
+	}
+	return &body, nil
+}
+
+func main() {
+
+	client, err := ethclient.Dial("http://ethermint0:8545")
+	if err != nil {
+		panic(err)
+	}
+	rawClient, err := rpc.DialHTTP("http://ethermint0:8545")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	header2, err := client.HeaderByNumber(ctx, big.NewInt(int64(2)))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("header2: ", header2)
+	fmt.Println("header2 hash: ", header2.Hash())
+
+	// i := 0
+	rawHeader2, err := getBlockHeaderByNum(rawClient, toBlockNumArg(big.NewInt(int64(2))), true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("rawHeader2: ", rawHeader2)
+	fmt.Println("rawHeader2 hash: ", rawHeader2.Hash())
+
+	// Start from chain genesis
+	// height := 0
+}
